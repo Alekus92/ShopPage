a2c256b34c36a958a9847bd52af737d8f8c9fa8c
Aleksander Scislowski <alekuscislowski@yahoo.pl>
2018-11-04 16:48:19 +0100

    updates

---
+++ my-app/src/components/pages/homePage.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Homepage extends Component {
+  render() {
+    return <div className="container-fluid">Home Page content here</div>;
+  }
+}
+
+export default Homepage;
